2754298a0b2dd3258443f4a37be82e5af029328a
Hanifc56 <abuhanif007sd@gmail.com>
2024-01-23 20:38:50 +0600

    Firest server build and intrigate wite client site

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <Link to="/phones">All Phones</Link>
+    </div>
+  );
+};
+
+export default Header;
